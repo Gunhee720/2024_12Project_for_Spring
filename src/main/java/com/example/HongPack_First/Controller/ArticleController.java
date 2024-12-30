8ad9c639f0351c83755cb8a970e1696a106ff391
Geon <jcv31312@naver.com>
2024-12-30 14:31:57 +0900

    feat:edit & delete 생성

--- src/main/java/com/example/HongPack_First/Controller/ArticleController.java
+++ src/main/java/com/example/HongPack_First/Controller/ArticleController.java
@@ -7,10 +7,10 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import java.util.List;
 
 @Controller
 @Slf4j //로깅을 위한 (어노테이션)
@@ -40,17 +40,53 @@ public class ArticleController {
         log.info("id = {}",id);
         //id로 data를 가져옴
         Article ArticleEntity_byid = articleRepository.findById(id).orElse(null);
-        log.info("myid = {}",ArticleEntity_byid);
+        log.info("myid = {}", ArticleEntity_byid);
         //가져온 데이터를 model에 등록
-        model.addAttribute("article",ArticleEntity_byid);
+        model.addAttribute("article", ArticleEntity_byid);
         //보여줄 페이지를 설정
         return "articles/show";
     }
 
     @GetMapping("/articles")
     public String ArticleList(Model model){
-        model.addAttribute("articles",articleRepository.findAll());
+        //모든 article을 가져온다
+        List<Article> ArticleList = articleRepository.findAll();
+        log.info("myAritcleList = {}", ArticleList);
+        //가져온 article들을 묶어서 뷰로 보냄
+        model.addAttribute("articles", ArticleList);
+        //뷰페이지
         return "articles/list";
+    }
+
+    @GetMapping("/articles/{id}/edit")
+    public String ArticleEdit(@PathVariable Long id, Model model){
+        Article ArticleEntity_byid = articleRepository.findById(id).orElse(null);
+        model.addAttribute("article", ArticleEntity_byid);
+        return "articles/edit";
+    }
+
+    @PostMapping("/articles/update")
+    public String ArticleUpdate(@ModelAttribute("Article") ArticleForm form){
+        log.info("form : {}",form);
+        Article myarticle = form.toEntity();
+        Article oldArticle = articleRepository.findById(myarticle.getId()).orElse(null);
+        if(oldArticle !=null){
+            Article saved = articleRepository.save(myarticle);
+            log.info("saved : {}",saved);
+        }
+        return "redirect:/article/"+myarticle.getId();
+
+    }
+    @GetMapping("/articles/{id}/delete")
+    public String ArticleDelete(@PathVariable Long id, RedirectAttributes redirectAttributes) {
+        Article ArticleEntity_byid = articleRepository.findById(id).orElse(null);
+        log.info("article : {}",ArticleEntity_byid);
+        if(ArticleEntity_byid !=null){
+            articleRepository.delete(ArticleEntity_byid);
+            log.info("삭제처리완료");
+            redirectAttributes.addFlashAttribute("msg","삭제가 완료되었습니다.");
+        }
+        return "redirect:/articles";
     }
 
 }
---
+++ src/main/resources/data.sql
@@ -0,0 +1,4 @@
+INSERT INTO ARTICLE(title,content) VALUES ('강건희','안녕하세요');
+INSERT INTO ARTICLE(title,content) VALUES ('이현룡','야');
+INSERT INTO ARTICLE(title,content) VALUES ('이요한','오~');
+INSERT INTO ARTICLE(title,content) VALUES ('더미','덤덤');
